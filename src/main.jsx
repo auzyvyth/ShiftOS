7d7d58e53075dae245e78e3880bd30ebc719dd48
Airy Auzy Daniel <auzyvyth@gmail.com>
2026-04-01 23:08:11 +0800

    yuh

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import './i18n/config';
 import App from '@/App';
 import { Toaster } from '@/components/ui/toaster';
 import '@/index.css';
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
+    <Suspense fallback={null}>
         <App />
         <Toaster />
-    </>
+    </Suspense>
 );
